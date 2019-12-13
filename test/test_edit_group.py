b9142b73160f537904427f543baf387e4a2e4939
aapambukyan <apambukyan@mont.com>
2019-12-13 14:20:53 +0300

    Промежуточные проверки сравнения списка

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,18 +3,28 @@ from model.group import Group
 
 
 def test_edit_first_group(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="new group", header="new group", footer="new group"))
     app.group.edit_first_group(Group(name="new edit group", header="new edit group", footer="new edit group"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_group_name(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="new group"))
     app.group.edit_first_group(Group(name="change new edit group"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_group_header(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(header="new group"))
     app.group.edit_first_group(Group(header="change new header group"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+
