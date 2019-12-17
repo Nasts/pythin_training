e9d70cdeb6bb1ae6c5df37cd48af4011d8e67863
aapambukyan <apambukyan@mont.com>
2019-12-17 09:54:52 +0300

    Исправила свои ошибки, теперь тесты работают

--- fixture/contact.py
+++ fixture/contact.py
@@ -19,6 +19,7 @@ class ContactHelper:
         self.fill_contact_form(contact)
         # submit contact creation
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+        self.contact_cache = None
 
     def change_field_value(self, field_name, text):
         wd = self.app.wd
@@ -29,8 +30,8 @@ class ContactHelper:
 
     def fill_contact_form(self, contact):
         wd = self.app.wd
-        self.change_field_value("firstname",contact.first_name)
         self.change_field_value("lastname", contact.last_name)
+        self.change_field_value("firstname", contact.first_name)
         self.change_field_value("address", contact.address)
         self.change_field_value("email", contact.email)
         self.change_field_value("mobile", contact.mobile)
@@ -49,6 +50,7 @@ class ContactHelper:
         # submit edit
         wd.find_element_by_name("update").click()
         self.return_to_home_page()
+        self.contact_cache = None
 
     def delete_first_contact(self):
         wd = self.app.wd
@@ -59,6 +61,7 @@ class ContactHelper:
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         # close dialog window
         wd.switch_to_alert().accept()
+        self.contact_cache = None
 
     def add_first_contact_to_group(self):
         wd = self.app.wd
@@ -76,25 +79,23 @@ class ContactHelper:
 
     def return_to_home_page(self):
         wd = self.app.wd
-        if not(wd.current_url.endswith('addressbook/') and len(wd.find_elements_by_name("add")) > 0):
+        if not (wd.current_url.endswith('addressbook/') and len(wd.find_elements_by_name("add")) > 0):
             wd.find_element_by_link_text("home").click()
 
+    contact_cache = None
 
     def get_contact_list(self):
-        wd = self.app.wd
-        self.return_to_home_page()
-        contacts = []
-        count = len(wd.find_elements_by_xpath('//*[@id="maintable"]/tbody/tr'))
-        for i in range(2,count):
-            element_name = wd.find_elements_by_xpath(f'//*[@id="maintable"]/tbody/tr[{i}]/td[2]')
-            text_name = element_name[0].text
-            id = wd.find_elements_by_xpath(f'//*[@id="maintable"]/tbody/tr[{i}]/td[1]/input')[0].get_attribute('value')
-            element_lastname = wd.find_elements_by_xpath(f'//*[@id="maintable"]/tbody/tr[{i}]/td[3]')
-            text_lastname = element_lastname[0].text
-            contacts.append(Contact(first_name=text_name,last_name=text_lastname,id=id))
-        return contacts
-
-
-
-
-
+        if self.contact_cache is None:
+            wd = self.app.wd
+            self.return_to_home_page()
+            self.contact_cache = []
+            count = len(wd.find_elements_by_xpath('//*[@id="maintable"]/tbody/tr'))
+            for i in range(2, count + 1):
+                object_last_name = wd.find_elements_by_xpath(f'//*[@id="maintable"]/tbody/tr[{i}]/td[2]')
+                text_last_name = object_last_name[0].text
+                id = wd.find_elements_by_xpath(f'//*[@id="maintable"]/tbody/tr[{i}]/td[1]/input')[0].get_attribute(
+                    'value')
+                object_first_name = wd.find_elements_by_xpath(f'//*[@id="maintable"]/tbody/tr[{i}]/td[3]')
+                text_first_name = object_first_name[0].text
+                self.contact_cache.append(Contact(last_name=text_last_name, first_name=text_first_name, id=id))
+        return list(self.contact_cache)
--- model/contact.py
+++ model/contact.py
@@ -3,19 +3,19 @@ from sys import maxsize
 
 class Contact:
 
-    def __init__(self, first_name=None, last_name=None, address=None, email=None, mobile=None, id=None):
-        self.first_name = first_name
+    def __init__(self, last_name=None,first_name=None,  address=None, email=None, mobile=None, id=None):
         self.last_name = last_name
+        self.first_name = first_name
         self.address = address
         self.email = email
         self.mobile = mobile
         self.id = id
 
     def __repr__(self):
-        return "%s:%s:%s" % (self.id, self.first_name, self.last_name)
+        return "%s:%s:%s" % (self.id,  self.last_name, self.first_name,)
 
     def __eq__(self, other):
-        return (self.id is None or other.id is None or self.id == other.id) and self.first_name == other.first_name and self.last_name == other.last_name
+        return (self.id is None or other.id is None or self.id == other.id) and self.last_name == other.last_name and self.first_name == other.first_name
 
     def id_or_max(self):
         if self.id:
