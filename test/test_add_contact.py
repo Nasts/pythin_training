d06b5f888ce4093c404cbb7377f3da119e680db9
aapambukyan <apambukyan@mont.com>
2019-12-25 16:00:35 +0300

    Задание №15: Параметризация тестов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,26 +1,26 @@
 # -*- coding: utf-8 -*-
-
+import pytest
 from model.contact import Contact
+import string
+import random
 
 
-def test_add_contact(app):
+# генератор случайных строк
+def random_string(prefix, max_len):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " " * 10  # символы в случайно сгенерированной строке
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(max_len))])
+
+testdata = [Contact(last_name="", first_name="", address="")] + [
+    Contact(last_name=random_string("first_name", 10), address=random_string(("header"), 20))
+    for i in range(5)
+]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(last_name="new_last name", first_name="new_firs_tname",  address="Moscow", email="test@msk.com",
-                      mobile_phone="123")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
-
-# def test_empty_contact(app):
-#     old_contacts = app.contact.get_contact_list()
-#     contact = Contact(first_name="", last_name=" name", address="", email="",
-#                       mobile="")
-#     app.contact.create(contact)
-#     new_contacts = app.contact.get_contact_list()
-#     assert len(old_contacts) + 1 == len(new_contacts)
-#     old_contacts.append(contact)
-#     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
