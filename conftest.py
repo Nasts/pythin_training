2553292bc8ff9b5e1f8338831d784a2f19d57d2d
aapambukyan <apambukyan@mont.com>
2019-12-25 21:01:08 +0300

    Задание №16 -

--- conftest.py
+++ conftest.py
@@ -8,13 +8,16 @@ fixture = None
 @pytest.fixture
 def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseUrl")
+    password = request.config.getoption("--password")
     if fixture is None:
         # создаем фикстуру, т.е. объект типа Application
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url, password=password)
     else:
-        if not fixture.is_valid():
+        if not fixture.is_valid(browser=browser, base_url=base_url, password=password):
             fixture = Application()
-    fixture.session.ensure_login(username="admin", password="secret")
+    fixture.session.ensure_login(username="admin", password=password)
     return fixture
 
 
@@ -25,3 +28,11 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--baseUrl", action="store", default="http://localhost:8080/addressbook/")
+    parser.addoption("--password", action="store", default=None)
+
+
--- fixture/application.py
+++ fixture/application.py
@@ -8,13 +8,21 @@ from fixture.session import SessionHelper
 
 class Application:
 
-    def __init__(self):
+    def __init__(self, browser, base_url, password):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.wd = webdriver.Chrome()
-        # self.wd = WebDriver()
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
-
+        self.base_url = base_url
+        self.password = password
     def is_valid(self):
         try:
             self.wd.current_url
@@ -24,7 +32,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost:8080/addressbook/")
+        wd.get(self.base_url)
 
 
     def destroy(self):
